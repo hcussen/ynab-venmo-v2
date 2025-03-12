4fc63c905dd264c8841a1f2c3446e7d8f986d3f3
Hannah Cussen <hcussen6@gmail.com>
2025-03-12 13:01:46 -0600

    test gh actions

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
           <li className="tracking-[-.01em]">
             Save and see your changes instantly.
           </li>
-          <li>Testing the github actions</li>
+          <li>Testing the github actions again</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
