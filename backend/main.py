fc60be469e531ebf33a098e509b98e0211398488
Hannah Cussen <hcussen6@gmail.com>
2025-03-12 12:22:16 -0600

    add supabase

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,23 @@
 from typing import Optional
-
 from fastapi import FastAPI
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    supabase_url: str
+    supabase_key: str
+    model_config = SettingsConfigDict(env_file=".env")
+
+
+settings = Settings()
+
+import os
+from supabase import create_client, Client
+
+url: str = settings.supabase_url
+key: str = settings.supabase_key
+supabase: Client = create_client(url, key)
+
 
 app = FastAPI()
 
@@ -8,6 +25,12 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/supabase")
+async def get_supabase():
+    response = supabase.table("planets").select("*").execute()
+    return response
 
 
 @app.get("/items/{item_id}")
