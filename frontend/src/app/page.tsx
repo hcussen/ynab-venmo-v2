e8b90627abe92e86d74c1fd18f14db535383e770
Hannah Cussen <hcussen6@gmail.com>
2025-04-12 15:55:09 -0600

    put authaware on landing page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,29 @@
-import { InfoIcon } from "lucide-react"
+import AuthAware from "@/components/AuthAware"
+import { Button } from "@/components/ui/button"
+import Link from "next/link"
 
 export default async function Home() {
+  const renderSignedIn = () => {
+    return (
+      <Link href="/protected">
+        <Button>Go to dashboard</Button>
+      </Link>
+    )
+  }
+  const renderSignedOut = () => {
+    return (
+      <Link href="/sign-up">
+        <Button>Sign Up</Button>
+      </Link>
+    )
+  }
   return (
     <>
       <h1>Welcome to YNAB-Venmo integration!</h1>
+      <AuthAware
+        renderSignedIn={renderSignedIn}
+        renderSignedOut={renderSignedOut}
+      />
     </>
   )
 }
